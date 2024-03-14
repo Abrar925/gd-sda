4d436f28434a0aad511b955f848b5c41da924853
Abrar925 <143460899+Abrar925@users.noreply.github.com>
2024-03-14 10:54:44 +0300

    3.3 Basic animations (part 1.1 - legacy animation)

---
+++ Assets/A-Scripts/AnimationEventHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventHandler : MonoBehaviour
+{
+    public void OnMyAnimatinEvent()
+    {
+        Debug.Log("Animation event happened");
+    }
+
+}
